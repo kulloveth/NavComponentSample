f5541f9e54e35fcc46b6229d93d128c552cbcde8
Kulloveth <kulloveth@gmail.com>
2020-01-03 04:12:46 +0100

    created a navigation from signup to login

--- app/src/main/java/com/developer/kulloveth/navcomponentsample/fragments/SignUpFragment.java
+++ app/src/main/java/com/developer/kulloveth/navcomponentsample/fragments/SignUpFragment.java
@@ -4,18 +4,21 @@ package com.developer.kulloveth.navcomponentsample.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.developer.kulloveth.navcomponentsample.R;
+import com.google.android.material.button.MaterialButton;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignUpFragment extends Fragment {
 
+    MaterialButton button;
 
     public SignUpFragment() {
         // Required empty public constructor
@@ -26,7 +29,12 @@ public class SignUpFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        final View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        button = view.findViewById(R.id.sign_up_button);
+        button.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_signUpFragment_to_loginFragment, null));
+
+        return view;
     }
 
 }
