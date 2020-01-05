2a5357a53c0e58365d31dcd5d4fd19e87522a899
Kulloveth <kulloveth@gmail.com>
2020-01-05 02:27:03 +0100

    set navigation to welcome sreen

--- app/src/main/java/com/developer/kulloveth/navcomponentsample/fragments/LoginFragment.java
+++ app/src/main/java/com/developer/kulloveth/navcomponentsample/fragments/LoginFragment.java
@@ -4,18 +4,21 @@ package com.developer.kulloveth.navcomponentsample.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.developer.kulloveth.navcomponentsample.R;
+import com.google.android.material.button.MaterialButton;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class LoginFragment extends Fragment {
 
+    MaterialButton button;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -26,7 +29,12 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        button = view.findViewById(R.id.login_button);
+        button.setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment,null)
+        );
+        return view;
     }
 
 }
